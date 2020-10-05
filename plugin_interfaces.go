2e5a77c83fbd4498e326221aa16a596b4faccf68
Andrew Mason <andrew@nocturnal.net.au>
2020-10-05 23:34:07 +1030

    Initial Commit

---
+++ plugin_interfaces.go
@@ -0,0 +1,24 @@
+package pipfire_interface
+
+import (
+    "fmt"
+)
+//PipefirePlugin Available operations for the plugin
+type PipefirePlugin interface {
+	GetVersion() string
+	GetName() string
+	IsPipeline() bool
+	IsExtention() bool
+}
+
+//Pipeline is the
+type Pipeline interface {
+	StartListener(listenerError chan error)
+	Execute(string) []error
+	Close() error
+}
+
+//FooBar
+func FooBar() {
+  fmt.Print("FooBar")
+}
